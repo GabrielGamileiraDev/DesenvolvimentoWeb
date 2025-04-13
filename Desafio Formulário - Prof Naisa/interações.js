7cff1d54dc66530cb375842b176358ee1f8f7366
Biel Dev <gabrielgamileira512@gmail.com>
2025-04-13 17:11:48 -0300

    Formulário - Prof Naisa
    Formulário para sistematizar uma biblioteca (pelo menos na parte de cadastrar livros).

--- Desafio Formulário - Prof Naisa/interações.js
+++ Desafio Formulário - Prof Naisa/interações.js
@@ -1,2 +1,3 @@
 console.log("Espero que tenha gostado do formulario.\n");
-console.log("Não sou bom em escolher cor. :/");
+console.log("Não sou bom em escolher cor. :/\n");
+console.log("Baixe os arquivos .ico e .pngpara que possa ver o conteúdo completo.");
